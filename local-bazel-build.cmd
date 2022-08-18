cc55795ee3428c6595a49df4f8bc2f882ddba276
dirkarnez <smalldirkalex@gmail.com>
2022-08-18 21:55:24 +0800

    - revamp project, wip bazel

---
+++ local-bazel-build.cmd
@@ -0,0 +1,23 @@
+
+
+
+REM run as Administrator
+@echo off
+
+cd /d %~dp0
+@REM set DOWNLOAD_DIR=%USERPROFILE%\Downloads
+@REM set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
+@REM SET PATH=^
+@REM %DOWNLOAD_DIR%\PortableGit\bin;^
+@REM %DOWNLOAD_DIR%\x86_64-8.1.0-release-win32-seh-rt_v6-rev0;^
+@REM %DOWNLOAD_DIR%\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\bin;^
+@REM %DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;^
+
+set PATH=^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+D:\Softwares\bazel-5.2.0-windows-x86_64;
+@REM --compiler=mingw-gcc
+bazel build --config=clang_config //src:main &&^
+echo "Successful build"
+pause
